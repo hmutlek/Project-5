5865a94b61291276496d050cb74cf48b0350d48e
tripping <benzhoui2016@gmail.com>
2021-07-30 14:10:34 -0400

    Update Application.java

--- Application.java
+++ Application.java
@@ -16,29 +16,31 @@ public class Application {
         }
         return false;
     }
-    public static void main (String[] args) {
+
+    public static void main (String[] args) throws IOException {
         String username;
         String password;
         choice = welcome();
+        Account user = new Account();
+
+
+        //the following codes are used to test login and signup options
         try {
             switch (choice) {
                 case "1" :
-                    Account user = new Account();
-                    boolean userCheck = false;
-                    do {
-                        userCheck = false;
-                        System.out.println("Please enter your username:");
-                        username = scan.nextLine();
-                        System.out.println("Please enter your password:");
-                        password = scan.nextLine();
-                        if (user.logIn(username, password)) {
-                            System.out.println("Logged in");
-                            userCheck = true;
-                        } else {
-                            System.out.println("Log in failed, please check your username or password");
-                        }
-                    } while (!userCheck);
-                    break;
+                    user = new Account();
+                    System.out.println("Please enter your username:");
+                    username = scan.nextLine();
+                    System.out.println("Please enter your password:");
+                    password = scan.nextLine();
+                    if (user.logIn(username, password)) {
+                        System.out.println("Logged in");
+                        user.getIdentifier();
+                        break;
+                    } else {
+                        System.out.println("Log in failed, please check your username or password");
+                        choice = welcome();
+                    }
 
 
                 case "2" :
@@ -46,22 +48,78 @@ public class Application {
                     username = scan.nextLine();
                     System.out.println("Please enter your password:");
                     password = scan.nextLine();
-                    Account newUser = new Account(username, password);
-                    if (newUser.signUp()) {
+                    user = new Account(username, password);
+                    if (user.signUp()) {
                         System.out.println("Signed up");
+                        user.getIdentifier();
+                        break;
                     } else {
                         System.out.println("Sign up failed, the username is already in use");
+                        choice = welcome();
                     }
+
+                case "exit" :
                     break;
+
+                default :
+                    System.out.println("please enter a valid input");
+                    choice = welcome();
             }
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("oops, IOException occurred.");
         }
 
+        System.out.println("What would you like to do?\n" +
+            "1. change username     2. change password      3.delete account");
+        choice = scan.nextLine();
 
-//        do {
-//            System.out.println();
-//        } while (!testIfExit());
+        //now it's testing account modification
+        try {
+            switch (choice) {
+                case "1" :
+                    System.out.println("please enter a new username");
+                    String temp = scan.nextLine();
+                    if (user.changeUserName(temp)) {
+                        System.out.println("username changed successfully.");
+                        System.out.println("What would you like to do?\n" +
+                            "1. change username     2. change password      3.delete account");
+                        choice = scan.nextLine();
+                    } else {
+                        System.out.println("username is already been used");
+                        System.out.println("What would you like to do?\n" +
+                            "1. change username     2. change password      3.delete account");
+                        choice = scan.nextLine();
+                    }
+
+                case "2" :
+                    System.out.println("please enter a new password");
+                    temp = scan.nextLine();
+                    if (user.changePassword(temp)) {
+                        System.out.println("password changed successfully.");
+                        System.out.println("What would you like to do?\n" +
+                            "1. change username     2. change password      3.delete account");
+                        choice = scan.nextLine();
+                    } else {
+                        System.out.println("password failed to be changed");
+                        System.out.println("What would you like to do?\n" +
+                            "1. change username     2. change password      3.delete account");
+                        choice = scan.nextLine();
+                    }
+
+                case "3" :
+                    System.out.println("Are you sure you want to change the password?");
+                    temp = scan.nextLine();
+                    if (temp.equalsIgnoreCase("yes")) {
+                        user.deleteAccount();
+                        System.out.println("account deleted");
+                        break;
+                    } else {
+                        System.out.println("going back...");
+                    }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
